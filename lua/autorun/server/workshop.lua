305b96f3a6c63af53cd9796a86cbf9bf86847f33
Maxime GUINARD <90180968+MaximeGuinard@users.noreply.github.com>
2022-01-29 18:24:10 +0100

    Add files via upload

---
+++ lua/autorun/server/workshop.lua
@@ -0,0 +1,7 @@
+resource.AddWorkshop("632470227") -- Example VCMod Content
+resource.AddWorkshop("2699793823") -- >>> https://steamcommunity.com/sharedfiles/filedetails/?id=  2699793823
+resource.AddWorkshop("1308262997") -- Example SNTE (Say No To Exploits - ONLY WORKSHOP COLLECTION)
+resource.AddWorkshop("859079882") -- Example Playermodel - Capitaine, Lieutenant et Commandant de Police
+resource.AddWorkshop("841919670") -- Example Playermodel - SAMU
+
+-- You add all your workshop collection 
